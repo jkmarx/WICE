d869229e18f9f1f2b1d9ae118e9e1766ec59e738
J.Marx <jennyrutledge@gmail.com>
2015-02-12 01:04:05 -0500

    Update config option for precompile assets.

--- config/application.rb
+++ config/application.rb
@@ -20,7 +20,7 @@ module WICEApp
     # The default locale is :en and all translations from config/locales/*.rb,yml are auto loaded.
     # config.i18n.load_path += Dir[Rails.root.join('my', 'locales', '*.{rb,yml}').to_s]
     # config.i18n.default_locale = :de
-    config.serve_static_files = true
+    config.serve_static_assets = true
 
     config.autoload_paths += %W(#{config.root}/lib)
     # Do not swallow errors in after_commit/after_rollback callbacks.
