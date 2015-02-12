19b7fdf0efed4145132a1404e8de0228c74e6a56
J.Marx <jennyrutledge@gmail.com>
2015-02-12 01:27:19 -0500

    Trying to fix the image issue and ratyrate stars.

--- config/application.rb
+++ config/application.rb
@@ -11,6 +11,7 @@ module WICEApp
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
+    config.assets.initialize_on_precompile=false
 
 
     # Set Time.zone default to the specified zone and make Active Record auto-convert to this zone.
