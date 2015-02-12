819909fc55c80635da154279c2fb1cb79268e43c
J.Marx <jennyrutledge@gmail.com>
2015-02-12 01:31:56 -0500

    Change the config/environments/production.rb to include config.serve_static_assets = true.

--- config/application.rb
+++ config/application.rb
@@ -11,7 +11,6 @@ module WICEApp
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
-    config.assets.initialize_on_precompile=false
 
 
     # Set Time.zone default to the specified zone and make Active Record auto-convert to this zone.
@@ -21,9 +20,12 @@ module WICEApp
     # The default locale is :en and all translations from config/locales/*.rb,yml are auto loaded.
     # config.i18n.load_path += Dir[Rails.root.join('my', 'locales', '*.{rb,yml}').to_s]
     # config.i18n.default_locale = :de
-    config.serve_static_assets = true
 
     config.autoload_paths += %W(#{config.root}/lib)
+
+    config.serve_static_assets = true
+
+
     # Do not swallow errors in after_commit/after_rollback callbacks.
     config.active_record.raise_in_transactional_callbacks = true
   end
